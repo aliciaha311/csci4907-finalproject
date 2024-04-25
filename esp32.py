2620956c6192f691d84f67461a796cd0b64890ff
aliciaha311 <56080552+aliciaha311@users.noreply.github.com>
2024-04-25 02:50:17 -0400

    Update esp32.py

--- esp32.py
+++ esp32.py
@@ -1,7 +1,6 @@
-import machine
 import time
-from machine import Pin, time_pulse_us, PWM
 from servo import Servo
+from machine import Pin
 
 soundSpeed = 340 # speed of sound
 trigDuration_US = 10 # trig duration
@@ -12,13 +11,18 @@ TRIG = Pin(23, Pin.OUT)
 ECHO = Pin(36, Pin.IN)
 
 # servo
-serv1 = machine.PWM(machine.Pin(15))
-serv1.freq(50)
+mini_servo = Servo(15)
+
+def move_servo():
+    mini_servo.move(90)
+    time.sleep(0.7)
+    mini_servo.move(0)
+    time.sleep(0.3)
 
 # calculates distance in centimeters
 def distance_calc():
     TRIG.value(0)
-    time.sleep_us(2)
+    time. sleep_us(2)
     TRIG.value(1)
     time.sleep_us(trigDuration_US)
     TRIG.value(0)
@@ -32,15 +36,14 @@ def distance_calc():
     time.sleep_ms(500)
     return during * soundSpeed / 2 * 100 # returns distance in centimeters
 
+'''
 try:
     while True:
         distance_calc()
         detectionDistance = distance_calc()
         print("Distance: " + str(detectionDistance))
-        if detectionDistance < 50:
-            serv1.duty_u16(2000)
-            time.sleep(1)
-            serv1.duty_u16(5000)
-            time.sleep(1)
+        if detectionDistance < 10:
+            move_servo()
 except KeyboardInterrupt:
     print("Program stopped")
+'''
